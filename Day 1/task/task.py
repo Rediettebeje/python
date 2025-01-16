9e86253e70dea072ab6025614719b03d21eac63f
Rediettebeje <131832561+Rediettebeje@users.noreply.github.com>
2025-01-15 22:40:32 -0600

    added comment

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -2,4 +2,5 @@ print("welcome")
 city = input("which city did you grow up in\n")
 pet = input("what is the name of a pet \n")
 print("Your band name could be " + city + " " + pet)
+# len is a function that returns the length of a string
 print(len(city))
