4840b643914f04b33e610e5ab6fa206d0ed98281
Rediettebeje <131832561+Rediettebeje@users.noreply.github.com>
2025-01-16 09:46:38 -0600

    day 2 learning

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,6 +1,7 @@
-print("welcome")
-city = input("which city did you grow up in\n")
-pet = input("what is the name of a pet \n")
-print("Your band name could be " + city + " " + pet)
-# len is a function that returns the length of a string
-print(len(city))
+# this method of pulling out a particular element from a string is called subscripting
+print("welcome"[0])
+# integer = whole number
+# type function allow us to check the data type of any piece od data, type checking
+print(type("hello"))
+# type conversion or type casting
+print(len("12345"))
