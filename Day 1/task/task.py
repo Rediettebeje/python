facaf84c8de2e8c7955672b78330f16872d4eebe
Rediettebeje <131832561+Rediettebeje@users.noreply.github.com>
2025-01-15 22:38:45 -0600

    day 1

---
+++ Day 1/task/task.py
@@ -0,0 +1,5 @@
+print("welcome")
+city = input("which city did you grow up in\n")
+pet = input("what is the name of a pet \n")
+print("Your band name could be " + city + " " + pet)
+print(len(city))
