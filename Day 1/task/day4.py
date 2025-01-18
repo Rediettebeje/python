2cd9675b301b0f8bce8e521e9ccaaed996acb77b
Rediet Tebeje <131832561+Rediettebeje@users.noreply.github.com>
2025-01-18 10:17:12 -0600

    returned a random element from the list

--- Day 1/task/day4.py
+++ Day 1/task/day4.py
@@ -28,3 +28,16 @@ if heads_or_tails >= 5:
     print("Heads")
 else:
     print("Tails")
+# how to pick a random name from the list of friends.
+
+friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+
+# option 1
+# random.choice(seq)
+# Return a random element from the non-empty sequence seq.
+# If seq is empty, raises IndexError.
+print(random.choice(friends))
+
+# option 2
+rand_friends = random.randint(0,4)
+print(friends[rand_friends])
