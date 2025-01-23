d1b8561e9ae5f83c3e56cd91b11ef6b795fe7cf5
Rediettebeje <131832561+Rediettebeje@users.noreply.github.com>
2025-01-22 22:59:29 -0600

    day7 learning

--- Day 1/task/day7.py
+++ Day 1/task/day7.py
@@ -1,13 +1,13 @@
 import random
 word_list = ["aardvark", "baboon", "camel"]
 
-# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
+# - Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
 
 chosen_word = random.choice(word_list)
 print(chosen_word)
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+# - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 guess = input("guess a letter").lower()
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it
+#- Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it
 #  is, "Wrong" if it's not.
 for char in chosen_word:
     if guess == char:
@@ -15,14 +15,14 @@ for char in chosen_word:
     else:
         print("wrong")
 
-# TODO-1: Create a "placeholder" with the same number of blanks as the chosen_word
+# Create a "placeholder" with the same number of blanks as the chosen_word
 placeholder = ""
 for letter in range(len(chosen_word)):
     placeholder += "-"
 print(placeholder)
 
 guess = input("Guess a letter: ").lower()
-# TODO-2: Create a "display" that puts the guess letter in the right positions and _ in the rest of the string.
+# Create a "display" that puts the guess letter in the right positions and _ in the rest of the string.
 
 display = ""
 for letter in chosen_word:
