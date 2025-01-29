8e0cda4740afca4db2d94328223533daae0d2af4
Rediettebeje <131832561+Rediettebeje@users.noreply.github.com>
2025-01-28 23:42:44 -0600

    fixed list index out of range by using %

--- Day 1/task/day8_caesarCipher.py
+++ Day 1/task/day8_caesarCipher.py
@@ -19,11 +19,16 @@ def encrypt(original_text,shift_amount):
     for letter in original_text:
         index_of_letter = alphabet.index(letter)
         new_position = index_of_letter + shift_amount
+        new_position %= len(alphabet) # 0-25
         encrypted_text += alphabet[new_position]
     print(encrypted_text)
-
 # What happens if you try to shift z forwards by 9? Can you fix the code?
-
+# If the first number (dividend) is smaller than the second number (divisor),
+# the modulus (%) will always be the first number itself
+#     new_position %= len(alphabet) # 0-25
+#    8 %= 25 == 8
+# If A < B, then A % B = A because the smaller number can’t fully fit into the larger one
+# The remainder is simply the smaller number since no division happens.
 
 # Call the 'encrypt()' function and pass in the user inputs. You should be able to test the code and encrypt a
 #  message.
