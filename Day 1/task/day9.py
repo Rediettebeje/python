4fedad5702cbcaba00e53ffbdb6f9f761acdb771
Rediettebeje <131832561+Rediettebeje@users.noreply.github.com>
2025-01-31 22:09:03 -0600

    added example of nesting a list and dictionary inside a dictionary

--- Day 1/task/day9.py
+++ Day 1/task/day9.py
@@ -54,3 +54,29 @@ for key in student_scores:
     else:
         student_grades[key] = "Fail"
 print(student_grades)
+
+# Nesting a List inside a Dictionary
+
+travel_log = {
+    "France": ["Paris", "Lille", "Dijon"],
+    "Germany": ["Stuttgart", "Berlin"],
+}
+
+#  to print out "Lille" from the nested List called travel_log
+print(travel_log["France"][1])
+
+nested_list = ["A", "B", ["C", "D"]]
+print(nested_list[2][1]) # to print D
+
+#print out "Stuttgart" from the following list:
+travel_log = {
+  "France": {
+    "cities_visited": ["Paris", "Lille", "Dijon"],
+    "total_visits": 12
+   },
+  "Germany": {
+    "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+    "total_visits": 5
+   },
+}
+print(travel_log["Germany"]["cities_visited"][2])
