a5ab9b6628128a52291e658dfe149b6a184d13f4
Rediettebeje <131832561+Rediettebeje@users.noreply.github.com>
2025-02-11 21:47:28 -0600

    added comment about random.sample() function

--- Day 1/task/day11Project.py
+++ Day 1/task/day11Project.py
@@ -1,5 +1,9 @@
 import random
 
+# cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+# random.sample() function select unique two random numbers
+# randomComputerNo = random.sample(cards, 2)
+
 def deal_card():
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     card = random.choice(cards)
