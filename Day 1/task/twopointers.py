2f335770445198d279b77671edd540005e4ce746
Rediettebeje <131832561+Rediettebeje@users.noreply.github.com>
2025-02-14 12:47:32 -0600

    modified the comment

--- Day 1/task/twopointers.py
+++ Day 1/task/twopointers.py
@@ -1,11 +1,11 @@
 
 # since the array is sorted , we can use two pointers,
 # we start with the first and last number 1 + 15 = 16 > 13
-# since sum is > target we move the right pointer 1 + 14 = 15 > 13
-# 1+ 9 = 10 < 13 so now we have to move the left pointer to increase the sum
+# since sum is > target we move the right pointer to left to decrease the sum 1 + 14 = 15 > 13
+# 1+ 9 = 10 < 13 so now we have to move the left pointer right  to increase the sum
 # 2 + 9 = 11 < 13, 4 + 9 = 13 = 13 so the numbers are 4 and 9 we know that since the array is sorted
-# there are not other numbers in the array that give us the sum because if we move forward our left pointer
-# the sum will be greater than the target the same with the right pointer since its decrease the sum
+# there are not other numbers in the array that add up to the target because if we move the left pointer forward
+# the sum will become greater than the target, similarly if we move the right pointer backward , the sum will decrease
 
 
 
